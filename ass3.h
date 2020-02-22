493a1a5e700d84a3dce277b8b183b517e9850692
significantrobot <61357920+significantrobot@users.noreply.github.com>
2020-02-22 18:57:19 +0200

    Add files via upload

---
+++ ass3.h
@@ -0,0 +1,6 @@
+//#define MAX 5
+double recEffiPow(double, int);
+double recPow(double, int);
+double iterPow(double, int);
+int isPermutation(int arr1[], int, int arr2[], int);
+void printArr2D(int arr[][MAX], int, int);
---
+++ main.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <time.h>
+#include "ass3.h"
+//#define MAX 5
+
+extern int counter;
+
+void powManager(double base, int exp) {
+    counter = 0;
+    double iter = iterPow(base, exp);
+    printf("iter: %g^%d = %g\n", base, exp, iter);
+    // printf("%d\n", counter);
+    counter = 0;
+    double rec  = recPow(base, exp);
+    printf("rec : %g^%d = %g\n", base, exp, rec);
+    // printf("%d\n", counter);
+    counter = 0;
+    double effi  = recEffiPow(base, exp);
+    printf("effi: %g^%d = %g\n", base, exp, effi);
+    // printf("%d\n", counter);
+}
+
+
+void arrManager() {
+	int arr[MAX][MAX] = {{1,2,3,4},{555,6,77,8},{9999,10,11111,12}};
+	printArr2D(arr, 3, 4);
+}
+
+
+void checkPerm(int arr1[], int size1, int arr2[], int size2) {
+    if (isPermutation(arr1, size1, arr2, size2)) {
+        printf("Yes!\n");
+    } else {
+        printf("No!\n");
+    }
+}
+
+void permManager() {
+	int arr1[MAX] = {1,2,3,4,5};
+    int arr2[MAX] = {5,3,2,1,4};
+   int size1 = sizeof(arr1) / sizeof(int);
+  int size2 = sizeof(arr2) / sizeof(int);
+    checkPerm(arr1, size1, arr2, size2);
+
+    arr2[0] = 0;
+    checkPerm(arr1, size1, arr2, size2);
+
+    for (int i = 0; i < size1; i++) {
+		arr1[i] = i;
+	}
+    for (int i = 0; i < size2; i++) {
+		arr2[i] = size2-1-i;
+	}
+    checkPerm(arr1, size1, arr2, size2);
+}
+
+
+int main() { 
+    powManager(1.5, 3);
+    powManager(0, 10);
+    powManager(2, 8);
+    powManager(0, -2);
+    arrManager();
+    permManager();
+}
